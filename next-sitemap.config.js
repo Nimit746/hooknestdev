3274d88003b2e1e26d2970803b16af7a57d65acd
Nimit746 <144600536+Nimit746@users.noreply.github.com>
2026-05-05 22:56:26 +0530

    `Added next-sitemap dependency and updated package-lock.json`

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hooknest.vercel.app',
+  generateRobotsTxt: true, // also generates robots.txt
+  sitemapSize: 7000,
+}
